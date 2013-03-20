caa9d3d71b2fdf58a0555c9d43bf295e3914bf0f
Juha Heimonen <heimojuh@gmail.com>
2013-03-20 22:49:02 +0200

    writing setups

--- tasks/lib/ghostproxy.js
+++ tasks/lib/ghostproxy.js
@@ -1,7 +1,20 @@
+var express  = require("express");
 
 
 var Server = function(options) {
     this.options = options;
+    this.app = express();
+}
+
+Server.prototype.start = function() {
+    this.app.use(express.static(process.cwd+"/"+this.options.base_dir));
+    if (this.options.proxy_host) {
+        this.setupProxy();
+    }
+}
+
+Server.prototype.setupProxy = function() {
+
 }
 
 exports.Server = Server;
--- test/ghostproxy_test.js
+++ test/ghostproxy_test.js
@@ -1,17 +1,53 @@
 'use strict';
 
-var grunt = require('grunt'),
-    expect = require('chai').expect,
-    ghostproxy = require("../tasks/lib/ghostproxy.js");
+var grunt       = require('grunt'),
+    expect      = require('chai').expect,
+    sinon       = require('sinon'),
+    express     = require('express'),
+    ghostproxy  = require("../tasks/lib/ghostproxy.js");
 
 describe('grunt-ghostproxy', function() {
+    var server;
+
+    beforeEach(function() {
+        server = new ghostproxy.Server({base_dir: "."});
     
+    });
     it("Grunt be okay", function() {
     
     });
 
     it("Server has attribute base_dir", function() {
-        var server = new ghostproxy.Server({base_dir: "."});
         expect(server.options.base_dir).to.be.equal(".");
     });
+    
+    it("Server has app property", function() {
+        expect(server.app).to.be.ok;
+    });
+    
+    it("sets up static server for given base_dir when start is called", function() {
+
+       var stub = sinon.stub(server.app, "use");
+       server.start();
+       expect(stub.calledWith(sinon.match(express.static))).to.be.ok;
+       stub.restore();
+
+    });
+    
+    it("calls setupProxy if proxy host is given", function() {
+        var stub = sinon.stub(ghostproxy.Server.prototype, "setupProxy");
+        server = new ghostproxy.Server({base_dir: ".", proxy_host: "foo"});
+        server.start();
+        expect(stub.called).to.be.ok;
+        stub.restore();
+    });
+    
+    it("does not call setupProxy if proxy host is not given", function() {
+        var stub = sinon.stub(ghostproxy.Server.prototype, "setupProxy");
+        server = new ghostproxy.Server({base_dir: "."});
+        server.start();
+        expect(stub.called).not.to.be.ok;
+        stub.restore();
+    });
+
 });
